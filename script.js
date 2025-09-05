f62e16e7e1b989d4d05e2eeba7294765d14838c8
mmoreiracf <soarest22@gmail.com>
2025-09-05 07:56:59 -0300

    aula inicial

---
+++ script.js
@@ -0,0 +1,12 @@
+const nome = 'matheus'
+const idade = 28
+
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`)
+console.log(typeof (nome))
+
+if (nome === 'matheus') {
+    console.log('O nome é matheus')
+}
+
+if (2 == '2')
+    console.log('2 é igual a 2')
