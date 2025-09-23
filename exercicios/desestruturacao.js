267e7a8892b6baabea6421e7f56541c065199aba
evacosta <evacosta.if@gmail.com>
2025-09-23 14:15:30 -0300

    exercicio

---
+++ exercicios/desestruturacao.js
@@ -0,0 +1,16 @@
+// Exercícios de Manipulação de Dados
+
+// Exercício 1: Catálogo de Filmes (Praticando `.map`)
+
+// Objetivo:Praticar a transformação de um array de objetos em um array de strings formatadas.
+
+// Cenário:Você tem um array de objetos, onde cada objeto representa um filme.
+
+
+const filmes = [
+  { titulo: 'O Poderoso Chefão', ano: 1972, nota: 9.2 },
+  { titulo: 'Forrest Gump', ano: 1994, nota: 8.8 },
+  { titulo: 'A Origem', ano: 2010, nota: 8.8 },
+  { titulo: 'O Senhor dos Anéis: O Retorno do Rei', ano: 2003, nota: 9.0 }
+];
+
